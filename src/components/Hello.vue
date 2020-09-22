6e041b8c8b7cad3aae1ac3a79c0e78b471106ae0
aripatan <ari01sa30@gmail.com>
2020-09-22 11:32:50 +0900

    hello_error

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -8,33 +8,27 @@
       <p>task:</p>
     </form>
     <div class="task-list">
-      <label class="task-list__item"
-              v-for="todo in todos">
-        <input type="checkbox"><button>EDIT</button>{{ todo.text }}
-      </label>
+      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-router</label>
+      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vuex</label>
+      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-loader</label>
+      <label class="task-list__item--checked"><input type="checkbox" checked><button>EDIT</button>awesome-vue</label>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      todos : [
-        {text : 'vue-router', done: false},
-        {text : 'vuex', done: false},
-        {text : 'vue-loeader', done: false},
-        {text : 'awesome-vue', done: true},
-      ]
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 @mixin flex-vender() {
   display: flex;
   display: -webkit-flex;
@@ -49,7 +43,11 @@ export default {
   &__item {
     width: 270px;
     text-align: left;
-    
+    $element: #{&};
+    &--checked {
+      @extend #{$element};
+      color: #85a6c6;
+    }
   }
 }
 </style>
